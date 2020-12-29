2b7baa4ff6e5ba797d4d22c0c74480b922f44fba
tobi-bams <teebams49@gmail.com>
2020-12-29 22:48:55 +0100

    ccompleted todo task feature

--- index.js
+++ index.js
@@ -27,19 +27,22 @@ modeIconChange.addEventListener("click", (evt) => {
 function todoUIDisplay(todo){
     let todoContainer = document.createElement("div");
     todoContainer.setAttribute("class", "todo");
-    todoContainer.addEventListener("mouseenter", todoMouseEnter)
+    todoContainer.addEventListener("mouseenter", todoMouseEnter);
+    todoContainer.addEventListener("mouseleave", todoMouseLeave);
     let radioCheckContainer = document.createElement("div");
+    radioCheckContainer.addEventListener("click", radioCheckerClick)
     radioCheckContainer.setAttribute("class", "radio");
-    let checkImage = document.createElement("img");
-    checkImage.setAttribute("src", "./images/icon-check.svg");
     let todoTextContainer = document.createElement("div");
     todoTextContainer.setAttribute("class", "todoTextContainer");
     let todoText = document.createElement("p");
+    todoText.setAttribute("class", "todoText")
+    todoText.addEventListener("click", changetodoStatus);
     todoText.textContent = todo;
     todoTextContainer.appendChild(todoText);
     let cancelTodoContainer = document.createElement("div");
     cancelTodoContainer.setAttribute("class", "cancelTodo");
     let cancelImage = document.createElement("img");
+    cancelImage.addEventListener("click", cancelTodo);
     cancelImage.setAttribute("src", "./images/icon-cross.svg");
     cancelTodoContainer.appendChild(cancelImage);
     todoContainer.appendChild(radioCheckContainer);
@@ -52,3 +55,81 @@ todoArray.forEach((todo) => {
     let todoItem = todo.todo;
     todoUIDisplay(todoItem);
 });
+
+todos.parentElement.nextElementSibling
+
+function todoMouseEnter(evt){
+    let radioBackground = document.createElement("div");
+    radioBackground.setAttribute("class", "radioBackgound");
+    evt.target.children[0].style.border = "none";
+    evt.target.children[0].style.background = "linear-gradient(to right, hsl(192, 100%, 67%), hsl(280, 87%, 65%))"
+    evt.target.children[0].appendChild(radioBackground);
+    evt.target.children[2].children[0].style.display = "flex";
+}
+
+function todoMouseLeave(evt){
+    evt.target.children[2].children[0].style.display = "none";
+    evt.target.children[0].removeChild(evt.target.children[0].children[0]);
+    evt.target.children[0].style.background = "transparent";
+    evt.target.children[0].style.border = "1px solid hsl(237, 14%, 26%)";
+}
+
+
+function changetodoStatus(evt){
+    evt.target.style.textDecoration = "line-through";
+    evt.target.setAttribute("class", "todoTextLight");
+    evt.target.removeEventListener("click", changetodoStatus);
+    evt.target.parentElement.parentElement.removeEventListener("mouseenter", todoMouseEnter);
+    evt.target.parentElement.parentElement.removeEventListener("mouseleave", todoMouseLeave);
+    evt.target.parentElement.previousElementSibling.removeChild(evt.target.parentElement.previousElementSibling.children[0]);
+    let checkImage = document.createElement("img");
+    checkImage.setAttribute("src", "./images/icon-check.svg");
+    evt.target.parentElement.previousElementSibling.appendChild(checkImage);
+    evt.target.parentElement.parentElement.addEventListener("mouseenter", todoCompletedMouseEnter);
+    evt.target.parentElement.parentElement.addEventListener("mouseleave", todoCompletedMouseLeave);
+    evt.target.parentElement.previousElementSibling.removeEventListener("click", radioCheckerClick);
+    let todoIndex = todoArray.findIndex((todo) => {
+        return evt.target.textContent == todo.todo;
+    })
+    todoArray[todoIndex].status = "completed";
+}
+
+function radioCheckerClick(evt){
+    evt.target.parentElement.nextElementSibling.children[0].style.textDecoration = "line-through";
+    evt.target.parentElement.nextElementSibling.children[0].setAttribute("class", "todoTextLight");
+    evt.target.parentElement.nextElementSibling.children[0].removeEventListener("click", changetodoStatus);
+    evt.target.parentElement.parentElement.removeEventListener("mouseenter", todoMouseEnter);
+    evt.target.parentElement.parentElement.removeEventListener("mouseleave", todoMouseLeave);
+    evt.target.parentElement.removeEventListener("click", radioCheckerClick);
+    let checkImage = document.createElement("img");
+    checkImage.setAttribute("src", "./images/icon-check.svg");
+    evt.target.parentElement.appendChild(checkImage);
+    let todoTask = evt.target.parentElement.nextElementSibling.children[0].textContent;
+    let todoIndex = todoArray.findIndex((todo)=> {
+        return todo.todo == todoTask;
+    })
+    todoArray[todoIndex].status = "completed";
+    evt.target.parentElement.parentElement.addEventListener("mouseenter", todoCompletedMouseEnter);
+    evt.target.parentElement.parentElement.addEventListener("mouseleave", todoCompletedMouseLeave);
+    evt.target.parentElement.removeChild(evt.target.parentElement.children[0]);
+}
+
+function todoCompletedMouseEnter(evt){
+    evt.target.children[2].children[0].style.display = "block";
+}
+
+function todoCompletedMouseLeave(evt){
+    evt.target.children[2].children[0].style.display = "none";
+}
+
+function cancelTodo(evt){
+    let todoText = evt.target.parentElement.previousElementSibling.children[0].textContent;
+    let todoIndex = todoArray.findIndex((todo) => {
+        return todo.todo == todoText;
+    })
+    todoArray.splice(todoIndex, 1);
+    todos.innerHTML = "";
+    todoArray.forEach((todo) => {
+        todoUIDisplay(todo.todo);
+    })
+}
